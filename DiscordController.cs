f410adf63d0dbc33ea709817ec43296c12139fa2
purepelmen <84836119+purepelmen@users.noreply.github.com>
2021-12-17 21:40:57 +0200

    New files (NET CORE)

---
+++ DiscordController.cs
@@ -0,0 +1,72 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using DiscordRPC.Message;
+using System;
+
+namespace DSM
+{
+    public class DiscordController
+    {
+		public bool Connected { get => client.IsDisposed == false && client != null; }
+		public RichPresence Presence { get => presence; }
+
+		public Action<string> OnLogging;
+
+		private DiscordRpcClient client;
+		private RichPresence presence;
+
+		public DiscordController()
+        {
+			presence = new RichPresence();
+		}
+
+		public void ConnectDiscord(string applicatioId)
+		{
+			client = new DiscordRpcClient(applicatioId);
+
+			client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+
+			client.OnReady += OnClientReady;
+			client.OnPresenceUpdate += OnPresenceUpdate;
+            client.OnError += OnClientError;
+            client.OnClose += OnClientClose;
+
+			client.Initialize();
+			client.SetPresence(presence);
+		}
+
+        public void Disconnect()
+        {
+			client.ClearPresence();
+			client.Dispose();
+        }
+
+		public void UpdatePresence()
+        {
+			if (Connected == false)
+				throw new NullReferenceException("Discord isn't connected");
+
+			client.SetPresence(presence);
+		}
+
+		private void OnClientReady(object sender, ReadyMessage args)
+		{
+			OnLogging?.Invoke($"Client ready {args.User.Username}");
+		}
+
+		private void OnPresenceUpdate(object sender, PresenceMessage args)
+		{
+			OnLogging?.Invoke("Presence updated");
+		}
+
+		private void OnClientError(object sender, ErrorMessage args)
+		{
+			OnLogging?.Invoke($"Client error ({args.Message})");
+		}
+		private void OnClientClose(object sender, CloseMessage args)
+		{
+			OnLogging?.Invoke($"Client closed (Reason: {args.Reason})");
+		}
+
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,64 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSM
+{
+    public partial class MainForm : Form
+    {
+        private DiscordController discord;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            discord = new DiscordController();
+            discord.OnLogging += OnLogging;
+        }
+
+        private void ConnectBtnClick(object sender, EventArgs e)
+        {
+            string appId = applicationIdField.Text;
+
+            if(string.IsNullOrWhiteSpace(appId))
+            {
+                MessageBox.Show("Type application ID!", "Error");
+                return;
+            }
+
+            discord.ConnectDiscord(appId);
+        }
+
+        private void UpdatePresenceBtnClick(object sender, EventArgs e)
+        {
+            if(discord.Connected == false)
+            {
+                MessageBox.Show("Not connected to Discord", "Error");
+                return;
+            }
+
+            discord.Presence.State = stateField.Text;
+            discord.Presence.Assets = new Assets()
+            {
+                LargeImageKey = largeImageKeyField.Text,
+                LargeImageText = largeImageTextField.Text
+            };
+
+            discord.UpdatePresence();
+        }
+
+        private void OnLogging(string text)
+        {
+            logsBox.Invoke(new Action(() => logsBox.Items.Add(text)));
+        }
+
+        private void OnFormClose(object sender, FormClosingEventArgs e)
+        {
+            discord.Disconnect();
+        }
+    }
+}
